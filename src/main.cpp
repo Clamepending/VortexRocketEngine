8cdae1355265cac83190cd05dd39b3266cdf79cb
Mark <sotaogata777@gmail.com>
2023-02-24 21:05:26 -0800

    added oxygen controls. Next is to install ignition and assign ignition pin

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,16 @@ const char* password =  "ggeznore";
   
 AsyncWebServer server(80);
   
-int relayPin = 33;
+int fuelPin = 12;
+int oxygenPin = 13;
+int ignitionPin; //TODO: connect ignition
   
 void setup(){
   
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW);
+  pinMode(fuelPin, OUTPUT);
+  digitalWrite(fuelPin, LOW);
+  pinMode(oxygenPin, OUTPUT);
+  digitalWrite(oxygenPin, LOW);
   
   Serial.begin(115200);
   
@@ -26,31 +30,97 @@ void setup(){
   
   Serial.println(WiFi.localIP());
   
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", "ok");
-    digitalWrite(relayPin, LOW);
-    Serial.println("recieved off command");
-  });
-  
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain","ok");
-    digitalWrite(relayPin, HIGH);
-    Serial.println("recieved on command");
-  });
-  
-  server.on("/toggle", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain","ok");
-    digitalWrite(relayPin, !digitalRead(relayPin));
-    Serial.println("recieved toggle command");
-  });
-  
-  server.on("/relay", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", String(digitalRead(relayPin)));
+  server.on("/print", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "I am alive!" + String(digitalRead(fuelPin)));
+    Serial.println("print yay");
   });
 
-  server.on("/print", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", String(digitalRead(relayPin)));
-    Serial.println("print yay");
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "ok");
+    digitalWrite(oxygenPin, LOW);
+    digitalWrite(fuelPin, LOW);
+    Serial.println("recieved off command");
+  });
+
+  //FUEL
+  
+  server.on("/fuelOn", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain","ok");
+    digitalWrite(fuelPin, HIGH);
+    Serial.println("recieved FUAL on command");
+  });
+
+  server.on("/1secFuelToggle", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", String(digitalRead(fuelPin)));
+    digitalWrite(fuelPin, HIGH);
+    delay(1000);
+    digitalWrite(fuelPin, LOW);
+  });
+
+  //OXYGEN
+
+  server.on("/oxygenOn", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain","ok");
+    digitalWrite(oxygenPin, HIGH);
+    Serial.println("recieved OXYGEN on command");
+  });
+
+  server.on("/1secOxygenToggle", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", String(digitalRead(fuelPin)));
+    digitalWrite(oxygenPin, HIGH);
+    delay(1000);
+    digitalWrite(oxygenPin, LOW);
+  });
+
+  //IGNITION
+
+  server.on("/IgnitionOn", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain","ok");
+    digitalWrite(ignitionPin, HIGH);
+    Serial.println("recieved IGNITION on command");
+  });
+
+  server.on("/1secOxygenToggle", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", String(digitalRead(fuelPin)));
+    digitalWrite(ignitionPin, HIGH);
+    delay(1000);
+    digitalWrite(ignitionPin, LOW);
+  });
+
+
+  //Fire
+
+  server.on("/1secGasFlow", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", String(digitalRead(fuelPin)));
+    for (int i = 3; i > 0; i--) {
+      Serial.println("Gas Flowing in: " + String(i));
+      delay(1000);
+    }
+    digitalWrite(oxygenPin, HIGH);
+    delay(300);
+    digitalWrite(fuelPin, HIGH);
+    delay(500);
+    digitalWrite(fuelPin, LOW);
+    delay(200);
+    digitalWrite(oxygenPin, LOW);
+  });
+
+  server.on("/1secHotFire", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", String(digitalRead(fuelPin)));
+    for (int i = 3; i > 0; i--) {
+      Serial.println("Gas Flowing in: " + String(i));
+      delay(1000);
+    }
+    // TRIGGER IGNITION
+    delay(1000);
+    digitalWrite(oxygenPin, HIGH);
+    delay(300);
+    digitalWrite(fuelPin, HIGH);
+    delay(500);
+    digitalWrite(fuelPin, LOW);
+    delay(200);
+    digitalWrite(oxygenPin, LOW);
+    // TURN IGNITION OFF
   });
   
   server.begin();
